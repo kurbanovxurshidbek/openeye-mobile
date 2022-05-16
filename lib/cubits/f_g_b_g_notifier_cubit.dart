69c05d7def45ba766c2ba23cd7102e1bc8547186
Mansur5835 <mansurgulomdota5835@gmail.com>
2022-05-16 11:14:54 +0500

    excess packages were deleted

--- lib/cubits/f_g_b_g_notifier_cubit.dart
+++
@@ -1,6 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:key_board_app/cubits/f_g_b_g_notifier_state.dart';
-
-class FGBGNotifierCubit extends Cubit<FGBGNotifierState> {
-  FGBGNotifierCubit() : super(FGBGNotifierState());
-}
