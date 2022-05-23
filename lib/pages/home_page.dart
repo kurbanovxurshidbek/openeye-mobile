02f73968ad5b256eb235e13520e73fdeb809bcf3
Mansur5835 <mansurgulomdota5835@gmail.com>
2022-05-23 12:25:56 +0500

    convert page added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -53,63 +53,66 @@ class _HomePageState extends State<HomePage> {
                           Color(0xFF443efc),
                           Color(0xFF52e5de),
                         ])),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.6,
-                          width: MediaQuery.of(context).size.width,
-                          child: GridView(
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              childAspectRatio: 1,
-                              mainAxisSpacing: 10,
-                              crossAxisSpacing: 10,
+                    child: SingleChildScrollView(
+                      physics: NeverScrollableScrollPhysics(),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height,
+                            width: MediaQuery.of(context).size.width,
+                            child: GridView(
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                childAspectRatio: 1,
+                                mainAxisSpacing: 10,
+                                crossAxisSpacing: 10,
+                              ),
+                              children: [
+                                itemGrid(
+                                    "keyboard".tr(),
+                                    const Icon(Icons.keyboard,
+                                        size: 30, color: Colors.white),
+                                    ItemOfFullGrid.KeybordItem,
+                                    context),
+                                itemGrid(
+                                    "text_in_image".tr(),
+                                    const Icon(Icons.camera_alt,
+                                        size: 30, color: Colors.white),
+                                    ItemOfFullGrid.TextInImageItem,
+                                    context),
+                                itemGrid(
+                                    "book".tr(),
+                                    const Icon(Icons.multitrack_audio,
+                                        size: 30, color: Colors.white),
+                                    ItemOfFullGrid.BookRecordingItem,
+                                    context),
+                                itemGrid(
+                                    "settings".tr(),
+                                    const Icon(
+                                      Icons.settings,
+                                      size: 30,
+                                      color: Colors.white,
+                                    ),
+                                    ItemOfFullGrid.SettingItem,
+                                    context),
+                                itemGrid(
+                                    "saved_books".tr(),
+                                    const Icon(
+                                      Icons.settings,
+                                      size: 30,
+                                      color: Colors.white,
+                                    ),
+                                    ItemOfFullGrid.ListOfSavedAudioBooks,
+                                    context),
+                              ],
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
                             ),
-                            children: [
-                              itemGrid(
-                                  "keyboard".tr(),
-                                  const Icon(Icons.keyboard,
-                                      size: 30, color: Colors.white),
-                                  ItemOfFullGrid.KeybordItem,
-                                  context),
-                              itemGrid(
-                                  "text_in_image".tr(),
-                                  const Icon(Icons.camera_alt,
-                                      size: 30, color: Colors.white),
-                                  ItemOfFullGrid.TextInImageItem,
-                                  context),
-                              itemGrid(
-                                  "book".tr(),
-                                  const Icon(Icons.multitrack_audio,
-                                      size: 30, color: Colors.white),
-                                  ItemOfFullGrid.BookRecordingItem,
-                                  context),
-                              itemGrid(
-                                  "settings".tr(),
-                                  const Icon(
-                                    Icons.settings,
-                                    size: 30,
-                                    color: Colors.white,
-                                  ),
-                                  ItemOfFullGrid.SettingItem,
-                                  context),
-                              itemGrid(
-                                  "saved_books".tr(),
-                                  const Icon(
-                                    Icons.settings,
-                                    size: 30,
-                                    color: Colors.white,
-                                  ),
-                                  ItemOfFullGrid.ListOfSavedAudioBooks,
-                                  context),
-                            ],
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
           ),
--- lib/views/full_grid_view.dart
+++ lib/views/full_grid_view.dart
@@ -1,19 +1,13 @@
 import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:key_board_app/constants/enums.dart';
 import 'package:key_board_app/cubits/for_image_find_text/img_find_text_cubit.dart';
-import 'package:key_board_app/cubits/for_read_audio_book/reading_audio_book_cubit.dart';
-import 'package:key_board_app/models/audio_model.dart';
+import 'package:key_board_app/pages/convertion_page.dart';
 import 'package:key_board_app/pages/settings_page.dart';
-import 'package:path_provider/path_provider.dart';
 import '../navigators/goto.dart';
 import '../pages/list_of_saved_books.dart';
-import '../pages/reading_audio_page.dart';
-import '../services/file_picker_service.dart';
-import '../services/http_service.dart';
 import 'bottom_sheets.dart';
 
 //home page item on tap fucntions
@@ -29,7 +23,7 @@ void itemGridOnPressed(ItemOfFullGrid itemOfGridHome, BuildContext context) {
       break;
     case ItemOfFullGrid.BookRecordingItem:
       {
-        getPdfTextAndPushReadingBookPage(context);
+        GOTO.push(context, ConvertionPage());
       }
       break;
     case ItemOfFullGrid.SettingItem:
@@ -52,30 +46,6 @@ void itemGridOnPressed(ItemOfFullGrid itemOfGridHome, BuildContext context) {
         GOTO.push(context, SavedBooksPage());
       }
       break;
-  }
-}
-
-//get pdf file and convert to text and goto push reading page
-getPdfTextAndPushReadingBookPage(BuildContext context) async {
-  List<String>? list = await FilePickerService.getTextFromPdfAndName(context);
-  Uint8List? uint8list;
-
-  if (list != null) {
-    uint8list = await Network.getAudioFromApi(list[0]);
-
-    if (uint8list != null) {
-      final tempDir = await getTemporaryDirectory();
-      File file = await File('${tempDir.path}/${list[1]}.mp3').create();
-      await file.writeAsBytes(uint8list);
-      AudioModel audioModel = AudioModel(name: list[1], path: file.path);
-
-      GOTO.push(
-          context,
-          ReadingPage(
-            listAudio: [audioModel],
-            startOnIndex: 0,
-          ));
-    }
   }
 }
 
